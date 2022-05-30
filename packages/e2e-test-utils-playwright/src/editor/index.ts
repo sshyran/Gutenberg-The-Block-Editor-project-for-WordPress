621887e2af260a312c96cef1dc622f802632f42a
ramonjd <ramonjd@gmail.com>
2022-05-30 15:50:34 +1000

    initial commit, pulling over site editor utils, basic scaffolding

--- packages/e2e-test-utils-playwright/src/editor/index.ts
+++ packages/e2e-test-utils-playwright/src/editor/index.ts
@@ -14,7 +14,12 @@ import { openDocumentSettingsSidebar } from './open-document-settings-sidebar';
 import { openPreviewPage } from './preview';
 import { selectBlocks } from './select-blocks';
 import { showBlockToolbar } from './show-block-toolbar';
-import { saveSiteEditorEntities } from './site-editor';
+import {
+	openGlobalStylesPanel,
+	openPreviousGlobalStylesPanel,
+	saveSiteEditorEntities,
+	toggleGlobalStyles,
+} from './site-editor';
 
 type EditorConstructorProps = {
 	page: Page;
@@ -57,8 +62,11 @@ export class Editor {
 	getEditedPostContent = getEditedPostContent;
 	insertBlock = insertBlock;
 	openDocumentSettingsSidebar = openDocumentSettingsSidebar;
+	openGlobalStylesPanel = openGlobalStylesPanel;
 	openPreviewPage = openPreviewPage;
+	openPreviousGlobalStylesPanel = openPreviousGlobalStylesPanel;
 	saveSiteEditorEntities = saveSiteEditorEntities;
 	selectBlocks = selectBlocks;
 	showBlockToolbar = showBlockToolbar;
+	toggleGlobalStyles = toggleGlobalStyles;
 }
